0d3bc0738523c20e7c0782d292041b37dfd01a97
thanh tuan <tranthanhtuan98py@gmail.com>
2019-09-15 17:17:37 +0700

    5:17 15/9/2019

--- jquery/05/main.js
+++ jquery/05/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-    
+    $('.tdkhoi').click(function(event) {
+    	$(this).toggleClass('xanh');
+    });;
+
+
+    // Code phần hiệu ứng ảnh
+    $('.anh a').fancybox();
+    return false;
 });
